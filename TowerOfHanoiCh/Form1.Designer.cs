b45ba6b973aed9f6b947822474d389db7d253e07
ArsenCherjo <95962000+ArsenCherjo@users.noreply.github.com>
2022-05-23 00:11:17 +0300

    Добавьте файлы проекта.

---
+++ TowerOfHanoiCh/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace TowerOfHanoiCh
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblCounter = new System.Windows.Forms.Label();
+            this.bPlay = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.DiskCount = new System.Windows.Forms.NumericUpDown();
+            this.lblMoves = new System.Windows.Forms.Label();
+            this.listMoves = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.DiskCount)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Location = new System.Drawing.Point(12, 59);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(869, 411);
+            this.panel1.TabIndex = 8;
+            // 
+            // lblCounter
+            // 
+            this.lblCounter.AutoSize = true;
+            this.lblCounter.Location = new System.Drawing.Point(375, 23);
+            this.lblCounter.Name = "lblCounter";
+            this.lblCounter.Size = new System.Drawing.Size(171, 13);
+            this.lblCounter.TabIndex = 12;
+            this.lblCounter.Text = "Минимальное количество ходов";
+            // 
+            // bPlay
+            // 
+            this.bPlay.Location = new System.Drawing.Point(240, 12);
+            this.bPlay.Name = "bPlay";
+            this.bPlay.Size = new System.Drawing.Size(113, 41);
+            this.bPlay.TabIndex = 11;
+            this.bPlay.Text = "Играть";
+            this.bPlay.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(21, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 16);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Количество дисков";
+            // 
+            // DiskCount
+            // 
+            this.DiskCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.DiskCount.Location = new System.Drawing.Point(161, 19);
+            this.DiskCount.Maximum = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            this.DiskCount.Minimum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.DiskCount.Name = "DiskCount";
+            this.DiskCount.Size = new System.Drawing.Size(48, 22);
+            this.DiskCount.TabIndex = 9;
+            this.DiskCount.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.DiskCount.ValueChanged += new System.EventHandler(this.DiskCount_ValueChanged);
+            // 
+            // lblMoves
+            // 
+            this.lblMoves.AutoSize = true;
+            this.lblMoves.Location = new System.Drawing.Point(902, 40);
+            this.lblMoves.Name = "lblMoves";
+            this.lblMoves.Size = new System.Drawing.Size(34, 13);
+            this.lblMoves.TabIndex = 14;
+            this.lblMoves.Text = "Ходы";
+            // 
+            // listMoves
+            // 
+            this.listMoves.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listMoves.FormattingEnabled = true;
+            this.listMoves.Location = new System.Drawing.Point(905, 63);
+            this.listMoves.Name = "listMoves";
+            this.listMoves.Size = new System.Drawing.Size(196, 407);
+            this.listMoves.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1172, 567);
+            this.Controls.Add(this.lblMoves);
+            this.Controls.Add(this.listMoves);
+            this.Controls.Add(this.lblCounter);
+            this.Controls.Add(this.bPlay);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DiskCount);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.DiskCount)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblCounter;
+        private System.Windows.Forms.Button bPlay;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown DiskCount;
+        private System.Windows.Forms.Label lblMoves;
+        private System.Windows.Forms.ListBox listMoves;
+    }
+}
+
---
+++ TowerOfHanoiCh/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TowerOfHanoiCh
+{
+    public partial class Form1 : Form
+    {
+
+        private List<string> moves = new List<string>();
+        private List<Disks> TowerDisks = new List<Disks>();
+
+        int _DiskCount = 3;
+        int diskHeight = 30;
+        int baseHeight = 20;//20 в высоту
+        public Form1()
+        {
+            InitializeComponent();
+            resetPanel();
+        }
+
+        private void bPlay_Click(object sender, EventArgs e)
+        {
+
+
+            resetPanel();
+            bPlay.Enabled = false;
+            moves.Clear();
+            listMoves.Items.Clear();                            // Очистить список ходов
+            int NumberOfDisks = _DiskCount;                     // Объявить количество дисков
+            //solveTower(NumberOfDisks);
+            listMoves.DataSource = null;
+            listMoves.DataSource = moves;                            // Решить башню
+            bPlay.Enabled = true;
+        }
+        private void populateDisks()
+        {
+            int ii = 1;
+            foreach (Disks disk in TowerDisks)
+            {
+                PictureBox panelBox = disk.box;
+                panelBox.BackColor = colorSelector(disk);
+                disk.width = 200 - (20 * ii);
+                panelBox.Width = disk.width;
+                panelBox.Height = diskHeight;
+                panelBox.Tag = disk.DiskNo;
+                panelBox.BorderStyle = BorderStyle.FixedSingle;
+                Point boxLocation = new Point(getDiskX(disk), (panel1.Height - baseHeight) - (diskHeight * ii++));
+                panelBox.Location = boxLocation;
+                disk.box = panelBox;
+                panel1.Controls.Add(disk.box);
+
+            }
+        }
+
+        private int getDiskX(Disks disk)
+        {
+            int X = 0;
+            int Peg = 0;
+            switch (disk.peg)
+            {
+                case 'A': Peg = 1; break;
+                case 'B': Peg = 2; break;
+                case 'C': Peg = 3; break;
+            }
+            X = ((panel1.Width / 4) * Peg) - (int)(disk.width / 2);
+
+            return X;
+        }
+        private Color colorSelector(Disks disk)
+        {
+            switch (disk.DiskNo)
+            {
+                case 1: return Color.Red;
+                case 2: return Color.OrangeRed;
+                case 3: return Color.Yellow;
+                case 4: return Color.Green;
+                case 5: return Color.Blue;
+                case 6: return Color.Purple;
+                case 7: return Color.LightBlue;
+                default: return Color.Black;
+            }
+        }
+        /// <summary>
+        /// Button click event to solve tower
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public static int GetMoveCount(int numberOfDisks)
+        {
+            double numberOfDisks_Double = numberOfDisks;
+            return (int)Math.Pow(2, numberOfDisks_Double) - 1;
+        }
+        private void resetPanel()
+        {
+            //Создать стержни 
+            setupTower();
+            panel1.Controls.Clear();
+            TowerDisks = Enumerable.Range(1, _DiskCount).Select(i => new Disks() { DiskNo = i, peg = 'A', box = new PictureBox() }).OrderByDescending(i => i.DiskNo).ToList();
+            //Поместите диски на панель
+            populateDisks();
+            //Установка начального текстового значения для наименьших возможных ходов
+            lblCounter.Text = string.Format("Минимальное количество ходов {0}", GetMoveCount(_DiskCount));
+
+        }
+        private void setBase()
+        {
+            SolidBrush sb = new SolidBrush(Color.RosyBrown);
+            Graphics g = panel1.CreateGraphics();
+            int topSpacing = 100;
+            int width = 20;
+            //Рисует нижнюю полосу
+            g.FillRectangle(sb, 0, panel1.Height - baseHeight, panel1.Width, baseHeight);
+            //Рисует 1 стержень
+            drawPeg(panel1, g, sb, 1, width, topSpacing);
+            //Рисует 2 стержень
+            drawPeg(panel1, g, sb, 2, width, topSpacing);
+            //Рисует 3 стержень
+            drawPeg(panel1, g, sb, 3, width, topSpacing);
+        }
+        private void setupTower()
+        {
+            panel1.Paint += delegate
+            {
+                setBase();
+            };
+        }
+        private void drawPeg(Panel canvas, Graphics g, SolidBrush sb, int pegNo, int pegWidth, int topSpacing)
+        {
+            g.FillRectangle(sb, ((int)(canvas.Width / 4) * pegNo) - (pegWidth / 2), topSpacing, pegWidth, canvas.Height - topSpacing);
+        }
+
+        private void DiskCount_ValueChanged(object sender, EventArgs e)
+        {
+            _DiskCount = (int)DiskCount.Value;
+        }
+    }
+}
